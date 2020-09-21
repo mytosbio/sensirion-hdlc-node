8fa31d666c2f8052d567781781fa33bcc56e8edb
Henry Miskin <hpgmiskin@gmail.com>
2020-09-21 12:38:30 +0100

    feat(encapsulation): Encode data with escape chars

--- src/encapsulation.test.ts
+++ src/encapsulation.test.ts
@@ -1,10 +1,68 @@
-import { encode } from "./encapsulation";
+import { __escapeByte, encode } from "./encapsulation";
 
 describe("Encapsulation", () => {
+    describe("escapeByte()", () => {
+        test.each([
+            [0x7e, 0x5e],
+            [0x7d, 0x5d],
+            [0x11, 0x31],
+            [0x13, 0x33],
+        ])("should escape %s to become %s", (byte: number, escaped: number) => {
+            expect(__escapeByte(byte)).toEqual(escaped);
+        });
+    });
     describe("encode()", () => {
-        test("should return a buffer", () => {
-            const encoded = encode(Buffer.from([]));
-            expect(encoded).toEqual(expect.any(Buffer));
+        test("should add start and end bytes", () => {
+            const data = [0x00, 0x24, 0x01, 0x03, 0xd7];
+            const expected = [0x7e, 0x00, 0x24, 0x01, 0x03, 0xd7, 0x7e];
+            expect(encode(data)).toEqual(expected);
+        });
+        test("should add start and end bytes complex", () => {
+            const data = [0x00, 0x33, 0x04, 0x00, 0xfa, 0x36, 0x08, 0x90];
+            expect(encode(data)).toEqual([
+                0x7e,
+                0x00,
+                0x33,
+                0x04,
+                0x00,
+                0xfa,
+                0x36,
+                0x08,
+                0x90,
+                0x7e,
+            ]);
+        });
+        test("should escape special characters", () => {
+            const data = [0x11, 0x33, 0x04, 0x00, 0xfa, 0x36, 0x08, 0x7f];
+            expect(encode(data)).toEqual([
+                0x7e,
+                0x7d,
+                0x31,
+                0x33,
+                0x04,
+                0x00,
+                0xfa,
+                0x36,
+                0x08,
+                0x7f,
+                0x7e,
+            ]);
+        });
+        test("should escape special characters", () => {
+            const data = [0x00, 0x33, 0x04, 0x00, 0x13, 0x36, 0x08, 0x77];
+            expect(encode(data)).toEqual([
+                0x7e,
+                0x00,
+                0x33,
+                0x04,
+                0x00,
+                0x7d,
+                0x33,
+                0x36,
+                0x08,
+                0x77,
+                0x7e,
+            ]);
         });
     });
 });
--- src/encapsulation.ts
+++ src/encapsulation.ts
@@ -1,10 +1,30 @@
+const START_BYTE = 0x7e;
+const STOP_BYTE = 0x7e;
+const ESCAPE_BYTE = 0x7d;
+
+const SPECIAL_BYTES = [0x7e, 0x7d, 0x11, 0x13];
+
+/**
+ * Escape a special byte by adding escape bit flipping bit 5
+ * @param byte - Special byte which should be transformed
+ * @returns Byte with bit 5 flipped
+ */
+export const __escapeByte = (byte: number): number => 0b100000 ^ byte;
+
 /**
  * Encode a frame with escape characters
  * @param frame - Unencoded frame of bytes
  * @returns Encoded bytes
  */
-export const encode = (frame: Buffer): Buffer => {
-    return Buffer.from("");
+export const encode = (frame: number[]): number[] => {
+    const escapedBytes = Array<number>().concat(
+        ...frame.map(byte =>
+            SPECIAL_BYTES.includes(byte)
+                ? [ESCAPE_BYTE, __escapeByte(byte)]
+                : [byte],
+        ),
+    );
+    return [START_BYTE, ...escapedBytes, STOP_BYTE];
 };
 
 /**
